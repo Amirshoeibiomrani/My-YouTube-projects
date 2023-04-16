409d0f19e8e18a4d3ffb249ed204e68c08927fa9
ARSO1995 <amirtahaomrany@gmail.com>
2023-04-16 19:23:49 +0330

    Eye user1

--- 3-SpongeBob-Login-Form/script.js
+++ 3-SpongeBob-Login-Form/script.js
@@ -1,1 +1,68 @@
-let rightEye = document.querySelector(".righteye");
+let rightEye = document.querySelector('righteye');
+let leftEye  = document.querySelector('leftEye');
+let userInput = document.querySelector('userInput');
+let userInput = document.querySelector('#passInput');
+
+////////////////LeftEye////////////
+let leftEyeTop = 75;
+let leftEyeLeft = 110;
+let leftEyePaddingLeft = 0;
+let leftEyePaddingTop = 0;
+
+/////////////RightEye////////////
+let rightEyeTop =75;
+let rightEyeLeft = 175;
+let rightEyePaddingLeft = 0;
+let rightEyePaddingTop = 0;
+
+const userInputFocus = () => {
+    console.log('userInputFocus');
+    let eyePosition = setInterval(() => {
+        if(leftEyeLeft ===60) {
+            clearInterval(eyePosition)
+        }
+        leftEye.style.top = leftEyeTop + 'px'
+        leftEye.style.left = leftEyeLeft = 'px'
+
+        left.style.top = leftEyeTop = 'px'
+        left.style.left = leftEyeLeft = 'px'
+ 
+        leftEyeTop --
+        leftEyeLeft ++
+        rightEyeTop --
+        rightEyeLeft --
+
+    },10)
+}
+const userInputBlur = () => {
+    console.log('userInputBlur');
+    let eyePosition = setInterval(() => {
+        if(leftEyeTop === 75) {
+            clearInterval(eyePosition)
+        }
+        leftEye.style.top = leftEyeTop + 'px'
+        leftEye.style.left = leftEyeLeft +'px'
+
+        rightEye.style.top = leftEyeTop + 'px'
+        rightEye.style.left =leftEyeLeft + 'px'
+
+        
+    })
+}
+const userKeyHandler = () => {
+    console.log('userKeyHandler');
+}
+const passInputFocus = () => {
+    console.log('passInputFocus');
+}
+const passInputBlur = () => {
+    console.log('passInputBlur');
+}
+
+userInput.addEventListener('focus',userInputFocus);
+userInput.addEventListener('blur',userInputBlur);
+userInput.addEventListener('keydown',userKeyHandler);
+
+passInputBlur.addEventListener('focus',passInputFocus);
+passInputBlur.addEventListener('blur',passInputBlur);
+
