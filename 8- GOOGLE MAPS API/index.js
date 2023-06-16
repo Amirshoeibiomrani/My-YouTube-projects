d34127725d9f7b76b2811356c01c7af43abb416f
Amirshoeibiomrani <amirtahaomrany@gmail.com>
2023-06-16 18:12:41 +0330

    update1

--- 8- GOOGLE MAPS API/index.js
+++ 8- GOOGLE MAPS API/index.js
@@ -1,12 +1,30 @@
 function initialize() {
     var mapOptions = {
-        //Z
+        //Zoom of map on start
         zoom:15,
-
+        //Initial Center Cordinates On Start (United States, NewYork)
         center:new google.maps.LatLng(40.7094, -74.0076),
-
+        // Type Of Map (Roadmap, Satellite, Hybrid, Terrain)
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-
+        // Minimum Zoom Of Map
         minZoom:2
-    }
+    };
+
+    // Create An Info Window To Display Location Info
+    var infoWindow = new google.maps.InfoWindow();
+
+    //Create A Marker For Example: Iran, Mashhad
+    var marker = new google.maps.Marker({
+        // Cordinates For United States, Florida  
+        position: new google.maps.LatLng(27.6988, -81.6486),
+        // Attach The Marker
+        map: map,
+        // Tooltip On Hover
+        
+    })
+
+
+
+
+
 }
