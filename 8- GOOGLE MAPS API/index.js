29d1eb691d7bd32396db3361ad94c2ae5cfcbbda
Amirshoeibiomrani <amirtahaomrany@gmail.com>
2023-06-16 17:58:30 +0330

    update

---
+++ 8- GOOGLE MAPS API/index.js
@@ -0,0 +1,12 @@
+function initialize() {
+    var mapOptions = {
+        //Z
+        zoom:15,
+
+        center:new google.maps.LatLng(40.7094, -74.0076),
+
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+        minZoom:2
+    }
+}
