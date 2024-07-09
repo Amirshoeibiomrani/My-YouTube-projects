5d3fa0102717948599ae29d27fceb1bcd875fef9
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-07-09 11:16:29 -0500

    Chess Game updating

---
+++ 10 - CHESS GAME APP/app.js
@@ -0,0 +1,25 @@
+const gameBoard =document.querySelector("#gameboard")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width =8
+
+const startPieces =[
+rook,knight,bishop,queen,king,queen,bishop,knight,rook,
+pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+"","","","","","","","",
+"","","","","","","","",
+"","","","","","","","",
+"","","","","","","","",
+pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
+rook,knight,bishop,queen,king,queen,bishop,knight,rook
+
+]
+
+
+
+function createBoard(){
+    startPieces.forEach((startPiece)=>{
+        const square= document.createElement('div')
+        square.classList.add('square')
+    })
+}
---
+++ 10 - CHESS GAME APP/pieces.js
@@ -0,0 +1,7 @@
+const king='<div class="piece" id="king"> </div> '
+const queen='<div class="piece" id="queen"> </div> '
+const rook= '<div class="piece" id="rook"> </div> '
+const bishop='<div class="piece" id="bishop"> </div> '
+const pawn='<div class="piece" id="pawn"> </div> '
+const knight='<div class="piece" id="knight"> </div> '
+ 
