f0b8273a27758b84e09dae99162f23552d19fd3b
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-07-31 10:44:49 -0500

    Finish but style contine

---
+++ 12 - Shorten URL/main.js
@@ -0,0 +1,19 @@
+const shortBtn= document.getElementById('short-btn')
+const refreshBtn= document.getElementById('refresh-btn')
+shortBtn.addEventListener('click',shortenUrl)
+function shortenUrl(){
+    var originalUrl = document.getElementById("originalUrl").value
+    var apiUrl= "https://tinyurl.com/api-create.php?url="+ encodeURIComponent(originalUrl)
+    shortenedUrlTextarea = document.getElementById("shortenedUrl")
+
+    fetch(apiUrl)
+    .then(response=>response.text())
+    .then(data=>{
+        shortenedUrlTextarea.value= data;
+    }).catch(error=>{
+        shortenedUrlTextarea.value="Error : Unable to shorten URL!"
+    })
+}
+reloadBtn.addEventListener('click',()=>{
+    location.reload();
+})
